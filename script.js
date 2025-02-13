357f3041d1585256bf14c0a073fcbbad999f9540
MhokWorawat <worawat.mhok@gmail.com>
2025-02-13 23:31:51 +0700

    Separate files for index, home, script, and styles

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-/* script.js */
 document.getElementById("openCardButton").addEventListener("click", function() {
     document.getElementById("cardBack").style.display = "flex";
+    document.getElementById("messageVideo").play();
     this.style.display = "none";
+});
+
+document.getElementById("closeCardButton").addEventListener("click", function() {
+    document.getElementById("cardBack").style.display = "none";
+    document.getElementById("openCardButton").style.display = "block";
 });
