1a2b01772ef69f23d035ab3f2725b0492fe2ef14
MhokWorawat <worawat.mhok@gmail.com>
2025-02-14 00:31:45 +0700

    Separate files for index, home, script, and styles

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const openCardButton = document.getElementById("openCardButton");
     const closeCardButton = document.getElementById("closeCardButton");
+    const cardFront = document.getElementById("cardFront");
     const cardBack = document.getElementById("cardBack");
     const video = document.getElementById("messageVideo");
 
@@ -8,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
         cardBack.style.display = "flex";
         setTimeout(() => {
             cardBack.style.transform = "rotateY(0deg)";
+            cardFront.style.display = "none";
         }, 50);
 
         video.style.display = "block";
@@ -19,6 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
         cardBack.style.transform = "rotateY(180deg)";
         setTimeout(() => {
             cardBack.style.display = "none";
+            cardFront.style.display = "flex";
             video.pause();
             video.style.display = "none";
             openCardButton.style.display = "block";
